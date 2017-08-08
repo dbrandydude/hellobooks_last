b354420f29d3f028eb9ae4fd1f2ab8f79a1af46c
Dbrandy <dbrandydude@gmail.com>
2017-08-08 19:23:23 +0100

    Modify api-route - Fix name conflict

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -5,6 +5,6 @@ import BooksController from '../controllers/book';
 const router = express.Router();
 
 /* POST add book */
-router.post('/books', BookController.add);
+router.post('/books', BooksController.add);
 
 export default router;
