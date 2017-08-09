93bed1e9fa3557402c9f9bf1994c404830175183
Dbrandy <dbrandydude@gmail.com>
2017-08-09 18:08:39 +0100

    Restructure app for continuous integration implementation

--- server/models/index.js
+++ server/models/index.js
@@ -5,7 +5,7 @@ import { Sequelize } from 'sequelize';
 
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require('../../src/db/config/config.json')[env];
+const config = require('../../server/db/config/config.json')[env];
 const db = {};
 
 let sequelize;
